bd9ef08375741ba069352fab31926295dfa3693d
SiblingRiot <113385409+SiblingRiot@users.noreply.github.com>
2023-03-24 14:07:54 -0400

    Update MMM-Compliment-Sender.js

--- MMM-Compliment-Sender.js
+++ MMM-Compliment-Sender.js
@@ -7,30 +7,42 @@
  * MIT Licensed.
  */
 
-Module.register("MMM-Compliment-Sender", {
+Module.register('MMM-MyKeyboardModule', {
   defaults: {
-    jsonFile: "/home/pi/MagicMirror/Connections/complimentsB.json"
+    jsonFilePath: '/home/pi/MagicMirror/Connections/compplimentsB.json',
+    messageType: 'anytime'
   },
 
-  start: function() {
-    // Do any initialization here
+  start: function () {
+    Log.info('MMM-MyKeyboardModule started');
+    this.sendSocketNotification('LOAD_JSON_DATA');
   },
 
-  notificationReceived: function(notification, payload, sender) {
-    if (notification === "SHOW_KEYBOARD") {
-      // Show the keyboard and handle input
+  getDom: function() {
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = "Type a message:";
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "messageInput");
+    input.setAttribute("maxlength", "50");
+    input.setAttribute("placeholder", "Type your message here");
+    wrapper.appendChild(input);
+    var button = document.createElement("button");
+    button.innerHTML = "Send";
+    button.addEventListener("click", () => {
+      var message = input.value.trim();
+      if (message.length > 0) {
+        this.sendSocketNotification('SAVE_MESSAGE', { type: this.config.messageType, message: message });
+        input.value = '';
+      }
+    });
+    wrapper.appendChild(button);
+    return wrapper;
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'JSON_DATA_LOADED') {
+      // do something with the loaded JSON data if needed
     }
-  },
-
-  writeJsonToFile: function(json) {
-    // Write the JSON object to the file specified in the configuration
-  },
-
-  readJsonFromFile: function() {
-    // Read the JSON object from the file specified in the configuration
-  },
-
-  keyboardInputHandler: function(input) {
-    // Handle the input from the keyboard
   }
 });
