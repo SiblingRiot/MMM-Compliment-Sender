f34bf7c93d0a65be2eeafacdf54b1bc72ca9ba1e
SiblingRiot <113385409+SiblingRiot@users.noreply.github.com>
2023-03-24 14:08:50 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -5,34 +5,57 @@
  * MIT Licensed.
  */
 
-const fs = require("fs");
-const path = require("path");
-
-const NodeHelper = require("node_helper");
+const NodeHelper = require('node_helper');
+const fs = require('fs');
 
 module.exports = NodeHelper.create({
-  start: function() {
-    console.log("Starting node helper for module [" + this.name + "]");
+  start: function () {
+    console.log('MMM-MyKeyboardModule helper started...');
   },
 
-  readJsonFromFile: function(filepath) {
-    const absolutePath = path.resolve(filepath);
-    const jsonString = fs.readFileSync(absolutePath, "utf8");
-    return JSON.parse(jsonString);
-  },
-
-  writeJsonToFile: function(filepath, data) {
-    const absolutePath = path.resolve(filepath);
-    const jsonString = JSON.stringify(data);
-    fs.writeFileSync(absolutePath, jsonString, "utf8");
-  },
-
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === "WRITE_JSON_TO_FILE") {
-      this.writeJsonToFile(payload.filepath, payload.data);
-    } else if (notification === "READ_JSON_FROM_FILE") {
-      const data = this.readJsonFromFile(payload.filepath);
-      this.sendSocketNotification("JSON_DATA", data);
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'LOAD_JSON_DATA') {
+      this.loadJSONData(payload);
+    } else if (notification === 'SAVE_MESSAGE') {
+      this.saveMessage(payload);
     }
+  },
+
+  loadJSONData: function (payload) {
+    var self = this;
+    var filePath = payload.jsonFilePath;
+    fs.readFile(filePath, 'utf8', function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        var jsonData = JSON.parse(data);
+        self.sendSocketNotification('JSON_DATA_LOADED', jsonData);
+      }
+    });
+  },
+
+  saveMessage: function (payload) {
+    var self = this;
+    var filePath = payload.jsonFilePath;
+    var messageType = payload.type;
+    var message = payload.message;
+    fs.readFile(filePath, 'utf8', function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        var jsonData = JSON.parse(data);
+        if (jsonData[messageType] === undefined) {
+          jsonData[messageType] = [];
+        }
+        jsonData[messageType].push(message);
+        fs.writeFile(filePath, JSON.stringify(jsonData), 'utf8', function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('Message saved: ' + message);
+          }
+        });
+      }
+    });
   }
 });
